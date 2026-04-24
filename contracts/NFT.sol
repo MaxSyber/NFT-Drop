0742a23ad6804430c1894a9922813742f56000d0
Brett Seibert <brs5241psu@gmail.com>
2026-04-23 22:15:47 -0400

    Minting NFTs

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,14 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
     string public baseURI;
+    string public baseExtension = ".json";
+
+    event Mint(uint256 amount, address minter);
 
     constructor(
         string memory _name, 
@@ -24,8 +28,34 @@ contract NFT is ERC721Enumerable, Ownable {
         baseURI = _baseURI;
     }
 
-    function mint() public {
+    function mint(uint256 _mintAmount) public payable {
+        require(block.timestamp >= allowMintingOn);
+        require(_mintAmount > 0);
+
+        require(msg.value >= cost * _mintAmount);
+
         uint256 supply = totalSupply();
-        _safeMint(msg.sender, supply +1 );
+        require(supply + _mintAmount <= maxSupply);
+        
+        for(uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+
+        emit Mint(_mintAmount, msg.sender);
+    }
+    
+    function tokenURI(uint256 _tokenId) public view virtual override returns(string memory) { 
+        require(_exists(_tokenId), 'token dose not exist');
+        return (string (abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)));
+    }
+
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount= balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for(uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);   
+        }
+
+        return tokenIds;
     }
 }
--- test/NFT.js
+++ test/NFT.js
@@ -13,23 +13,22 @@ describe('Token', () => {
     const symbol = 'DP'
     const cost =ether(10)
     const maxSupply = 25
-    const releaseDate = '1893499200'
     const BASE_URI = 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
     
-
   beforeEach(async () => {
-    const NFT = await ethers.getContractFactory('NFT')
-    nft = await NFT.deploy('Dapp Punks', 'DP', cost, maxSupply, releaseDate, BASE_URI)
-
     accounts = await ethers.getSigners()
     deployer = accounts[0]
     minter = accounts[1]
-
   })
 
   describe('Deployment', () => {
-   
 
+    beforeEach(async () => {
+    const releaseDate = Date.now().toString().slice(0, 10)
+    const NFT = await ethers.getContractFactory('NFT')
+    nft = await NFT.deploy('Dapp Punks', 'DP', cost, maxSupply, releaseDate, BASE_URI)
+    })
+    
     it('Has Correct Name', async () => {
       expect(await nft.name()).to.equal(name)
     })
@@ -47,6 +46,7 @@ describe('Token', () => {
     })
 
     it('It Returns Correct Minting Time', async () => {
+      const releaseDate = Date.now().toString().slice(0, 10)
       expect(await nft.allowMintingOn()).to.equal(releaseDate)
     })
     
@@ -57,7 +57,6 @@ describe('Token', () => {
     it('Returns Correct Owner of Contract', async () => {
       expect(await nft.owner()).to.equal(deployer.address)
     })
-
   })
 
 
@@ -65,32 +64,86 @@ describe('Token', () => {
     let amount, transaction, result
 
     describe('Success', () => {
-
+      
       beforeEach(async () => {
-        amount = tokens(100)
-        transaction = await token.connect(deployer).transfer(receiver.address, amount)
+        const releaseDate = Date.now().toString().slice(0, 10)
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy('Dapp Punks', 'DP', cost, maxSupply, releaseDate, BASE_URI)
+        
+        transaction = await nft.connect(minter).mint(1, {value: cost })
         result = await transaction.wait()
       })
 
-      it('transfers token balances', async () => {
-        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
-        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      it('Returns Address of Minter', async () => {
+        expect(await nft.ownerOf(1)).to.equal(minter.address)
+      })
+
+      it('Returns Total Number of tokens the Minter Owns', async () => {
+        expect(await nft.balanceOf(minter.address)).to.equal(1)
+      })
+
+      it('Returns IPFS URI', async () => {
+        expect(await nft.tokenURI(1)).to.equal(`${BASE_URI}1.json`)
+      })
+
+      it('Updates the Total Supply', async () => {
+        expect(await nft.totalSupply()).to.equal(1)
+      })
+
+      it('Updates the Contract Ether Balance', async () => {
+        expect(await ethers.provider.getBalance(nft.address)).to.equal(cost)
+      })
+      it('Emits Mint Event', async () => {
+        await expect(transaction).to.emit(nft, 'Mint').withArgs(1, minter.address)
       })
 
     })
 
     describe('Failure', () => {
-      it('rejects insufficient balances', async () => {
-        const invalidAmount = tokens(100000000)
-        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+      beforeEach(async () => {
+        const releaseDate = new Date('May 26, 2030 18:00:00').getTime().toString().slice(0, 10)
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy('Dapp Punks', 'DP', cost, maxSupply, releaseDate, BASE_URI)
+      })
+      it('Rejects Insufficient Payment', async () => {
+        await expect(nft.connect(minter).mint(1, {value: ether (1) })).to.be.reverted
       })
 
-      it('rejects invalid recipent', async () => {
-        const amount = tokens(100)
-        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      it('Rejects Minting Before Allowed Time', async () => {
+        const releaseDate = new Date('May 26, 2030 18:00:00').getTime().toString().slice(0, 10)
+        await expect(nft.connect(minter).mint(1, {value: cost })).to.be.reverted
+      })
+
+      it('Requires At Least One NFT to be Minted', async () => {
+        await expect(nft.connect(minter).mint(0, {value: cost })).to.be.reverted
+      })
+
+      it('Rejects NFT Overminting if Above Max Supply', async () => {
+        await expect(nft.connect(minter).mint(100, {value: cost })).to.be.reverted
+      })
+      it('Dose not Return URIs for Invalid Tokens', async () => {
+        await expect(nft.tokenURI('99')).to.be.reverted
       })
 
     })
+  })
 
+  describe('Displaying NFTs', () => {
+    
+
+    it('Returns all the NFTs for a Given Owner', async () => {
+        let amount, transaction, result
+        const releaseDate = Date.now().toString().slice(0, 10)
+        const NFT = await ethers.getContractFactory('NFT')
+        nft = await NFT.deploy('Dapp Punks', 'DP', cost, maxSupply, releaseDate, BASE_URI)
+        
+        transaction = await nft.connect(minter).mint(3, {value: ether(30) })
+        result = await transaction.wait()
+        let tokenIds = await nft.walletOfOwner(minter.address)
+        expect(tokenIds.length).to.equal(3)
+        expect(tokenIds[0].toString()).to.equal('1')
+        expect(tokenIds[1].toString()).to.equal('2')
+        expect(tokenIds[2].toString()).to.equal('3')
+      })
   })
 })
